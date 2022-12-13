fa2d2256278f744b949fc5c6ff6ea958b82dacbc
Michael-Chacon <michaelchacon02@gmail.com>
2022-12-13 10:10:39 -0500

    Creé la vacante con livewire

--- app/Http/Livewire/CrearVacante.php
+++ app/Http/Livewire/CrearVacante.php
@@ -2,12 +2,62 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\Categoria;
 use App\Models\Salario;
 use Livewire\Component;
+use App\Models\Categoria;
+use App\Models\Vacante;
+use Illuminate\Contracts\Session\Session;
+use Livewire\WithFileUploads;
 
 class CrearVacante extends Component
 {
+    public $titulo;
+    public $salario;
+    public $categoria;
+    public $empresa;
+    public $ultimo_dia;
+    public $descripcion;
+    public $imagen;
+
+    use WithFileUploads;
+
+    protected $rules = [
+        'titulo' => 'required|string', 
+        'salario' => 'required',
+        'categoria' => 'required',
+        'empresa' => 'required',
+        'ultimo_dia' => 'required',
+        'descripcion' => 'required',
+        'imagen' => 'required|image|max:2058',
+    ];
+
+    public function crearVacante()
+    {
+        $datos = $this->validate();
+
+        // Almacenar la imagen
+        $imagen = $this->imagen->store('public/vacantes');
+        $nombre_imagen = str_replace('public/vacantes/', '', $imagen);
+
+        // Crear la vacante 
+        Vacante::create([
+            'titulo' => $datos['titulo'],
+            'salario_id' => $datos['salario'] ,
+            'categoria_id' => $datos['categoria'],
+            'empresa' => $datos['empresa'],
+            'ultimo_dia' => $datos['ultimo_dia'],
+            'descripcion' => $datos['descripcion'],
+            'imagen' => $nombre_imagen,
+            'user_id' => auth()->user()->id,
+        ]);
+
+        // Mostrar mensaje
+        session()->flash('mensaje', 'La vacante se publico correctamente');
+
+        // redireccionar al usuario 
+        return redirect()->route('vacantes.index');
+    }
+
     public function render()
     {
         // consultar DB
--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,52 +1,59 @@
-<form action="" class="md:w-1/2 space-y-5" enctype="multipart/form-data" method="POST">
+<form action="" class="md:w-1/2 space-y-5" wire:submit.prevent='crearVacante' enctype="multipart/form-data">
     <div>
         <x-input-label for="titulo" :value="__('Titulo vacante:')" />
-        <x-text-input id="titulo" class="block mt-1 w-full" type="text" name="titulo" :value="old('titulo')" placeholder="Titulo vacante" />
+        <x-text-input id="titulo" class="block mt-1 w-full" type="text" wire:model="titulo" :value="old('titulo')" placeholder="Titulo vacante" />
         <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
     </div>
 
     <div>
         <x-input-label for="salario" :value="__('Salario de la vacante:')" />
-        <select name="salario" id="salario" class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+        <select wire:model="salario" id="salario" class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
             <opton>-- selecciones -- </opton>
             @foreach ($salarios as $salario)
                 <option value="{{ $salario->id }}">{{ $salario->salario }}</option>
             @endforeach)
         </select>
+        <x-input-error :messages="$errors->get('salario')" class="mt-2" />
     </div>
 
     <div>
         <x-input-label for="categoria" :value="__('Categoria:')" />
-        <select name="categoria" id="categoria" class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+        <select wire:model="categoria" id="categoria" class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
             <opton>-- selecciones -- </opton>
             @foreach ($categorias as $id => $categoria)
                 <option value="{{ $id }}">{{ $categoria }}</option>
             @endforeach)
         </select>
+        <x-input-error :messages="$errors->get('categoria')" class="mt-2" />
     </div>
 
     <div>
         <x-input-label for="empresa" :value="__('Nombre de la empresa:')" />
-        <x-text-input id="empresa" class="block mt-1 w-full" type="text" name="empresa" :value="old('empresa')" placeholder="Empresa: ejm. Netflix, Facebook, Laravel" />
+        <x-text-input id="empresa" class="block mt-1 w-full" type="text" wire:model="empresa" :value="old('empresa')" placeholder="Empresa: ejm. Netflix, Facebook, Laravel" />
         <x-input-error :messages="$errors->get('empresa')" class="mt-2" />
     </div>
 
     <div>
         <x-input-label for="ultimo_dia" :value="__('Último día para postularse:')" />
-        <x-text-input id="ultimo_dia" class="block mt-1 w-full" type="date" name="ultimo_dia" :value="old('ultimo_dia')"/>
+        <x-text-input id="ultimo_dia" class="block mt-1 w-full" type="date" wire:model="ultimo_dia" :value="old('ultimo_dia')"/>
         <x-input-error :messages="$errors->get('ultimo_dia')" class="mt-2" />
     </div>
 
     <div>
         <x-input-label for="descripcion" :value="__('Descripción del trabajo:')" />
-        <textarea name="" id="descripcion" name="descripcion" cols="30" rows="10" placeholder="Descripción general del puesto, experiencia"
+        <textarea id="descripcion" wire:model="descripcion" cols="30" rows="10" placeholder="Descripción general del puesto, experiencia"
         class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">{{ old('descripcion') }}</textarea>
         <x-input-error :messages="$errors->get('descripcion')" class="mt-2" />
     </div>
 
     <div>
         <x-input-label for="imagen" :value="__('Imagen:')" />
-        <x-text-input id="imagen" class="block mt-1 w-full" type="file" name="imagen"/>
+        <x-text-input id="imagen" class="block mt-1 w-full" type="file" accept="image/*" wire:model="imagen"/>
+        <div class="my-5 w-80 flex items-center">
+            @if ($imagen)
+                <img src="{{ $imagen->temporaryUrl() }}" alt="">
+            @endif
+        </div>
         <x-input-error :messages="$errors->get('imagen')" class="mt-2" />
     </div>
 
