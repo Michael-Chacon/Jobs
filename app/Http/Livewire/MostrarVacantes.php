7783c54e67f270bd07e11654a1a8b6825b61999b
Michael-Chacon <michaelchacon02@gmail.com>
2022-12-15 11:33:25 -0500

    Eliminar vacante con Livewire y SweetAlert2

--- app/Http/Livewire/MostrarVacantes.php
+++ app/Http/Livewire/MostrarVacantes.php
@@ -7,6 +7,19 @@ use Livewire\Component;
 
 class MostrarVacantes extends Component
 {
+    // // Enunciar las funciones que usan eventos 
+    // protected $listeners = ['prueba'];
+
+    // public function prueba($vacante_id){
+    //     dd($vacante_id);
+    // }
+    protected $listeners = ['eliminarVacante'];
+
+    public function eliminarVacante(Vacante $vacante)
+    {
+        $vacante->delete();
+    }
+        
     public function render()
     {
         $vacantes = Vacante::where('user_id', auth()->user()->id)->paginate(10);
--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,31 +1,60 @@
-<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-    @forelse ($vacantes as $vacante)
-    <div class="p-6 text-gray-900 md:flex md:justify-between md:items-center">
-        <div class="space-y-3">
-            <a href="#" class="text-xl">
-                {{ $vacante->titulo }}
-            </a>
-            <p class="text-sm  text-gray-600">{{ $vacante->empresa }}</p>
-            <p>Ultimo día: {{ $vacante->ultimo_dia->format('d/m/Y') }}</p>
+<div>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        @forelse ($vacantes as $vacante)
+        <div class="p-6 text-gray-900 md:flex md:justify-between md:items-center">
+            <div class="space-y-3">
+                <a href="#" class="text-xl">
+                    {{ $vacante->titulo }}
+                </a>
+                <p class="text-sm  text-gray-600">{{ $vacante->empresa }}</p>
+                <p>Ultimo día: {{ $vacante->ultimo_dia->format('d/m/Y') }}</p>
+            </div>
+            <div class="flex flex-col md:flex-row items-stretch gap-3 text-center mt-5 md:mt-0">
+                <a href="#" class="uppercase bg-slate-800 px-4 rounded-lg text-white text-center  ">
+                    Candidatos
+                </a >
+                <a href="{{ route('vacantes.edit', $vacante->id) }}" class="uppercase bg-blue-800 px-4 rounded-lg text-white text-center  ">
+                    Editar
+                </a>
+                <button wire:click="$emit('mostrarAlerta', {{ $vacante->id }})" class="uppercase bg-red-600 px-4 rounded-lg text-white text-center  ">
+                    Eliminar
+                </button>
+            </div>
         </div>
-        <div class="flex flex-col md:flex-row items-stretch gap-3 text-center mt-5 md:mt-0">
-            <a href="#" class="uppercase bg-slate-800 px-4 rounded-lg text-white text-center  ">
-                Candidatos
-            </a>
-            <a href="{{ route('vacantes.edit', $vacante->id) }}" class="uppercase bg-blue-800 px-4 rounded-lg text-white text-center  ">
-                Editar
-            </a>
-            <a href="#" class="uppercase bg-red-600 px-4 rounded-lg text-white text-center  ">
-                Eliminar
-            </a>
-        </div>
+            <hr>
+            @empty
+            <p class="p-3 text-center text-sm text-gray-600"> No hay vacantes</p>
+        @endforelse
     </div>
-        <hr>
-        @empty
-        <p class="p-3 text-center text-sm text-gray-600"> No hay vacantes</p>
-    @endforelse
+    <div class="mt-10">
+        {{ $vacantes->links() }}
+    </div>
 </div>
 
-<div class="mt-10">
-    {{ $vacantes->links() }}
-</div>
+@push('scripts')
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        Livewire.on('mostrarAlerta', vacanteId => {
+            Swal.fire({
+                title: '¿Eliminar vacante?',
+                text: "Una vacante eliminada no se puede recuperar!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, eliminar!',
+                cancelButtonText: 'Cancelar'
+            }).then((result) => {
+            if (result.isConfirmed) {
+                    // Eliminar vacante desde el componente
+                    Livewire.emit('eliminarVacante', vacanteId),
+                    Swal.fire(
+                    'Eliminada!',
+                    'La vacante a sido eliminada.',
+                    'success'
+                    )
+            }
+            })
+        })
+    </script>
+@endpush
